40fd875328c6e63c48a6834c2dcb05a69c86b96f
Atul <atulkumarrayy@gmail.com>
2024-10-11 09:03:01 +0530

    tic.js

--- tic.js
+++ tic.js
@@ -69,7 +69,7 @@ const enableBoxes = () => {
 };
 
 const showWinner = (winner) => {
-  msg.innerText = `Congratulations, Winner is ${winner} and Win Atul`;
+  msg.innerText = `Congratulations, Winner is ${winner} `;
   msgContainer.classList.remove("hide");
   disableBoxes();
 };
